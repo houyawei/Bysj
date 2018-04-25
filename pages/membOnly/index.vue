6c815f2259f12d7dd0c15e44a3bd0a4c8183072a
fanjingjing <fanjingjing>
2018-04-25 18:18:35 +0800

    会员专区页面初稿

--- pages/membOnly/index.vue
+++ pages/membOnly/index.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="list">
     <div class="list-one">
-      <img src="../../static/coffer.jpg" class="coffer fl">
+      <img src="../../static/coffee.jpg" class="coffee fl">
       <div class="fl text">
         <h3>大夫撒旦撒打算</h3>
         <p>大家分开了速度加快风电技术看来飞机开绿灯三分房间打扫了房间都开始了就分开了速度范德萨减肥的克里斯就分开了速度机卡分离的数据库了房间卢卡斯的范德萨</p>
       </div>
       <button class="fl">￥ 1.00</button>
+    </div>
+    <div class="list-one">
+      <img src="../../static/mianbao.jpg" class="mianbao fl"/>
+      <div class="fl text">
+        <h3>吐司面包</h3>
+        <p>用养乐多乳酸发酵的酸奶代替水制作出这款吐司，采用零基础也能学会的采中种法发酵，带来如白棉花般轻盈柔软的口感，组织 Q 弹绵密，味道清淡爽口不甜腻，空口吃都停不下来。做早餐、下午茶或者小小垫饥都是不错的选择。</p>
+      </div>
+      <button class="fl">￥ 2.00</button>
+    </div>
+    <div class="list-one">
+      <img src="../../static/junkfood.jpg" class="junkfood fl"/>
+      <div class="fl text">
+        <h3>不节食,健康瘦</h3>
+        <p>资深美食和健身达人 Sunny 将在本专栏以图文的形式，透过 36 个美味菜谱与你分享她的健康饮食和科学健身经验，帮助你从思维和认知层面上理解健康生活的理念。知道了该吃什么、该怎么吃，自然可以放心吃美食，健康享“瘦”啦！</p>
+      </div>
+      <button class="fl">￥ 3.00</button>
+    </div>
+    <div class="list-one">
+      <img src="../../static/zaocan.jpg" class="zaocan fl"/>
+      <div class="fl text">
+        <h3>早餐计划</h3>
+        <p>北京大福将在本专栏中以图文的形式，每天提供简单、丰盛、营养的早餐搭配和食谱，连续 21 天不重样；并通过梳理早餐制作流程，帮你学会高效的早餐时间管理。</p>
+      </div>
+      <button class="fl">￥ 4.00</button>
     </div>
   </div>
 </template>
@@ -20,6 +44,10 @@
     width: 1000px;
     margin: 0 auto;
     margin-top: 30px;
+  }
+  .list-one{
+    margin-bottom: 20px;
+    overflow: hidden;
   }
   .list-one img{
     width: 100px;
